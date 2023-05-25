3265353f06d9748f065d9535c0a3fdcf99f23a4f
GYUTAE PARK <64678476+doraemon500@users.noreply.github.com>
2023-05-25 16:58:21 +0900

    서버 Connector 코드 수정

--- Login_Form.cs
+++ Login_Form.cs
@@ -23,8 +23,6 @@ namespace _2023AppSWClient
         private static Thread rcvThread;
         private static Thread sndThread;
         public static NetworkStream stream;
-        public static StreamReader reader;
-        public static StreamWriter writer;
 
         public Login_Form()
         {
@@ -75,9 +73,6 @@ namespace _2023AppSWClient
             {
                 MessageBox.Show(ex.Message);
             }
-
-
-
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -95,70 +90,5 @@ namespace _2023AppSWClient
         {
 
         }
-
-        private void ClientToServer_Connector()
-        {
-            try
-            {
-                client = new TcpClient();
-                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
-                int port = 12000;
-                client.Connect(localAddr, port);
-
-                stream = client.GetStream();
-                rcvThread = new Thread(new ThreadStart(ReceiverThread));
-                sndThread = new Thread(new ThreadStart(SendThread));
-
-                rcvThread.Start();
-                sndThread.Start();
-            }
-            catch (Exception e) { }
-        }
-
-        static void ReceiverThread()
-        {
-            try
-            {
-                reader = new StreamReader(stream);
-
-                while (true)
-                {
-                    String order = reader.ReadLine();
-                    Console.WriteLine(order);
-
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception : {0}", e);
-            }
-            finally
-            {
-                Console.WriteLine("연결중 문제가 발생했습니다");
-            }
-        }
-        static void SendThread()
-        {
-            try
-            {
-                writer = new StreamWriter(stream);
-
-                while (true)
-                {
-                    String order = Console.ReadLine();
-                    writer.WriteLine(order);
-                    writer.Flush();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception : {0}", e);
-            }
-            finally
-            {
-                Console.WriteLine("연결중 문제가 발생했습니다");
-            }
-        }
-
     }
 }
